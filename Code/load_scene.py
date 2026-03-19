7ecc4a3e5488c60848cb286564e6958c712735d6
John Hall <johnk.hall329@gmail.com>
2026-03-19 14:40:11 -0400

    Getting setup

---
+++ Code/load_scene.py
@@ -0,0 +1,17 @@
+import bpy
+import numpy as np
+import json
+
+# LOAD SCENE FROM JSON FILE
+# EXECUTED BY BLENDER PYTHON SCRIPT
+# IF EDITING IN BLENDER SAVE SCRIPT AFTER EDITING
+
+def load_from_json(file_path):
+    bpy.ops.mesh.primitive_cube_add(enter_editmode=False, align='WORLD', location=(-2.89929, 1.5, 0.05), scale=(1, 1, 1))
+    pass
+
+
+if __name__ == '__main__':
+    print("rendering")
+    load_from_json("")
+    
